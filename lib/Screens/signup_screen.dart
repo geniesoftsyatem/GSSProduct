1f659bf02fd5cc686a5a26f602944cd280f8cc65
ankitmauryagenie <ankitmaurya.genie@gmail.com>
2021-12-12 13:48:00 +0530

    network.dart dependencies signup_screen.dart

--- lib/Screens/signup_screen.dart
+++ lib/Screens/signup_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:genie_shield/Model/registration_model.dart';
 import 'package:genie_shield/Screens/signin_screen.dart';
+import 'package:genie_shield/utils/network.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({Key? key}) : super(key: key);
@@ -16,9 +21,12 @@ class _SignupScreen extends State<SignupScreen> {
   final TextEditingController _email_controller = TextEditingController();
   final TextEditingController _mobile_controller = TextEditingController();
   final TextEditingController _password_controller = TextEditingController();
-  final TextEditingController _confirm_password_controller = TextEditingController();
+  final TextEditingController _confirm_password_controller =
+      TextEditingController();
 
-  Future<RegistrationModel>? _futureAlbum;
+  Future<RegistrationModel>? _registrationResponse;
+
+  NetworkCall networkCall = NetworkCall();
 
   @override
   Widget build(BuildContext context) {
@@ -38,47 +46,51 @@ class _SignupScreen extends State<SignupScreen> {
                   children: [
                     SizedBox(
                       width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.height/7,
+                      height: MediaQuery.of(context).size.height / 7,
                     ),
                     Container(
                       margin: const EdgeInsets.all(10.0),
                       child: Align(
                         alignment: const Alignment(-1, 1),
                         child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              const Text(
-                                "Existing user?",
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            const Text(
+                              "Existing user?",
+                              style: TextStyle(
+                                  color: Color(0xFF111111),
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const SignInScreen()));
+                              },
+                              child: const Text(
+                                "LOGIN",
                                 style: TextStyle(
-                                    color: Color(0xFF111111),
-                                    fontSize: 14.0,
-                                    fontWeight: FontWeight.bold),
+                                  color: Color(0xFFFFAE00),
+                                  fontSize: 16.0,
+                                ),
                               ),
-                              ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(builder: (context) => const SignInScreen()));
-                                  },
-                                  child: const Text(
-                                    "LOGIN",
-                                    style: TextStyle(
-                                      color: Color(0xFFFFAE00),
-                                      fontSize: 16.0,
-                                    ),
-                                  ),
-                                  style: ElevatedButton.styleFrom(
-                                    fixedSize: Size(width, 30.0),
-                                    primary: const Color(0xFF111111),
-                                    shadowColor: const Color(0xFF111111),
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20.0),
-                                    ),
-                                    padding: const EdgeInsets.only(
-                                        left: 12.0, right: 12.0),
-                                  ))
-                            ]),
+                              style: ElevatedButton.styleFrom(
+                                fixedSize: Size(width, 30.0),
+                                primary: const Color(0xFF111111),
+                                shadowColor: const Color(0xFF111111),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                ),
+                                padding: const EdgeInsets.only(
+                                    left: 12.0, right: 12.0),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
@@ -115,88 +127,134 @@ class _SignupScreen extends State<SignupScreen> {
                           ),
                           const SizedBox(height: 20),
                           TextField(
-                            obscureText: true,
                             style: const TextStyle(color: Color(0xFFFFAE00)),
                             decoration: InputDecoration(
-                                hintText: 'Enter Email',
-                                hintStyle:
-                                    const TextStyle(fontSize: 14, color: Colors.grey),
-                                fillColor: const Color(0xFF3A3A3A),
-                                filled: true,
-                                contentPadding: const EdgeInsets.only(
-                                    left: 16.0, right: 16.0),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(20),
-                                )),
+                              hintText: 'Enter Email',
+                              hintStyle: const TextStyle(
+                                  fontSize: 14, color: Colors.grey),
+                              fillColor: const Color(0xFF3A3A3A),
+                              filled: true,
+                              contentPadding: const EdgeInsets.only(
+                                  left: 16.0, right: 16.0),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            controller: _email_controller,
                           ),
                           const SizedBox(height: 10),
                           TextField(
                             obscureText: true,
                             style: const TextStyle(color: Color(0xFFFFAE00)),
                             decoration: InputDecoration(
-                                hintText: 'Mobile number',
-                                hintStyle:
-                                    const TextStyle(fontSize: 14, color: Colors.grey),
-                                fillColor: const Color(0xFF3A3A3A),
-                                filled: true,
-                                contentPadding: const EdgeInsets.only(
-                                    left: 16.0, right: 16.0),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(20),
-                                )),
+                              hintText: 'Mobile number',
+                              hintStyle: const TextStyle(
+                                  fontSize: 14, color: Colors.grey),
+                              fillColor: const Color(0xFF3A3A3A),
+                              filled: true,
+                              contentPadding: const EdgeInsets.only(
+                                  left: 16.0, right: 16.0),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            keyboardType: TextInputType.phone,
+                            controller: _mobile_controller,
+                            inputFormatters: [
+                              LengthLimitingTextInputFormatter(10)
+                            ],
                           ),
                           const SizedBox(height: 10),
                           TextField(
                             obscureText: true,
                             style: const TextStyle(color: Color(0xFFFFAE00)),
                             decoration: InputDecoration(
-                                hintText: 'Password',
-                                hintStyle:
-                                    const TextStyle(fontSize: 14, color: Colors.grey),
-                                fillColor: const Color(0xFF3A3A3A),
-                                filled: true,
-                                contentPadding: const EdgeInsets.only(
-                                    left: 16.0, right: 16.0),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(20),
-                                )),
+                              hintText: 'Password',
+                              hintStyle: const TextStyle(
+                                  fontSize: 14, color: Colors.grey),
+                              fillColor: const Color(0xFF3A3A3A),
+                              filled: true,
+                              contentPadding: const EdgeInsets.only(
+                                  left: 16.0, right: 16.0),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            controller: _password_controller,
                           ),
                           const SizedBox(height: 10),
                           TextField(
-                            obscureText: true,
                             style: const TextStyle(color: Color(0xFFFFAE00)),
                             decoration: InputDecoration(
-                                hintText: 'Re-confirm Password',
-                                hintStyle:
-                                    const TextStyle(fontSize: 14, color: Colors.grey),
-                                fillColor: const Color(0xFF3A3A3A),
-                                filled: true,
-                                contentPadding: const EdgeInsets.only(
-                                    left: 16.0, right: 16.0),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(20),
-                                )),
+                              hintText: 'Re-confirm Password',
+                              hintStyle: const TextStyle(
+                                  fontSize: 14, color: Colors.grey),
+                              fillColor: const Color(0xFF3A3A3A),
+                              filled: true,
+                              contentPadding: const EdgeInsets.only(
+                                  left: 16.0, right: 16.0),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                            ),
+                            controller: _confirm_password_controller,
                           ),
                           const SizedBox(height: 10),
                           ElevatedButton(
-                              onPressed: () {},
-                              child: const Text(
-                                "SIGN UP",
-                                style: TextStyle(
-                                  color: Color(0xFF111111),
-                                  fontSize: 16.0,
-                                ),
+                            onPressed: () {
+                              if (_email_controller.text.isNotEmpty) {
+                                if (_mobile_controller.text.isNotEmpty) {
+                                  if (_password_controller.text.isNotEmpty) {
+                                    if (_confirm_password_controller
+                                        .text.isNotEmpty) {
+                                      if (_password_controller.text ==
+                                          _confirm_password_controller.text) {
+                                        String request_json =
+                                            jsonEncode(<String, String>{
+                                          "email": _email_controller.text,
+                                          "alt_mobile_no":
+                                              _mobile_controller.text,
+                                          "password": _password_controller.text,
+                                          "code": "",
+                                          "code_name": ""
+                                        });
+                                        _registrationResponse = networkCall.fetchPosts(_email_controller.text, _mobile_controller.text, _password_controller.text, context);
+                                      } else {
+                                        _createToast("Password does not match");
+                                      }
+                                    } else {
+                                      _createToast(
+                                          "Please enter re-confirm password");
+                                    }
+                                  } else {
+                                    _createToast("Please enter password");
+                                  }
+                                } else {
+                                  _createToast("Please enter mobile number");
+                                }
+                              } else {
+                                _createToast("Please enter email id");
+                              }
+                            },
+                            child: const Text(
+                              "SIGN UP",
+                              style: TextStyle(
+                                color: Color(0xFF111111),
+                                fontSize: 16.0,
                               ),
-                              style: ElevatedButton.styleFrom(
-                                fixedSize: Size(width, 30.0),
-                                primary: const Color(0xFFFFAE00),
-                                shadowColor: const Color(0xFFFFAE00),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20.0),
-                                ),
-                                padding: const EdgeInsets.only(
-                                    left: 12.0, right: 12.0),
-                              )),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              fixedSize: Size(width, 30.0),
+                              primary: const Color(0xFFFFAE00),
+                              shadowColor: const Color(0xFFFFAE00),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0),
+                              ),
+                              padding: const EdgeInsets.only(
+                                  left: 12.0, right: 12.0),
+                            ),
+                          ),
                           const SizedBox(height: 10),
                         ],
                       ),
@@ -204,12 +262,19 @@ class _SignupScreen extends State<SignupScreen> {
                   ),
                 ],
               ),
-              //color: const Color(0xFFFFAE00),
             ),
           ],
         ),
       ),
     );
+  }
+
+  void _createToast(String message) {
+    Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1);
   }
 }
 
--- lib/utils/network.dart
+++ lib/utils/network.dart
@@ -1,27 +1,73 @@
 import 'dart:convert';
-import 'dart:js';
 
+import 'package:flutter/cupertino.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:genie_shield/Model/registration_model.dart';
 import 'package:http/http.dart' as http;
 
 class NetworkCall {
-
-  Future<RegistrationModel> fetchPosts(JsObject request) async {
-    final response = await http.post(Uri.parse('https://jsonplaceholder.typicode.com/posts/'),
+  Future<RegistrationModel> fetchPosts(String email, String mobile_no,
+      String password, BuildContext context) async {
+    final response = await http.post(
+      Uri.parse(
+          'http://143.110.176.111/genieshield/index.php/Api/user_registeration'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
-    body: request
+      body: jsonEncode(<String, String>{
+        "email": email,
+        "alt_mobile_no": mobile_no,
+        "password": password,
+        "code": "",
+        "code_name": ""
+      }),
     );
 
     if (response.statusCode == 200) {
-      // If the server did return a 200 OK response,
-      // then parse the JSON.
+
+     // String response_server = json.decode(response.body);
+
+      _createToast("User registered successfully, Please login");
+      Navigator.of(context).pop();
       return RegistrationModel.fromJson(json.decode(response.body));
     } else {
-      // If the server did not return a 200 OK response,
-      // then throw an exception.
+      _createToast("Failed to register User");
       throw Exception('Failed to load album');
     }
+  }
+
+  // Future<RegistrationModel> fetchPosts(String email, String mobile_no,
+  //     String password, BuildContext context) async {
+  //   final response = await http.post(
+  //     Uri.parse(
+  //         'http://143.110.176.111/genieshield/index.php/Api/user_registeration'),
+  //     headers: <String, String>{
+  //       'Content-Type': 'application/json; charset=UTF-8',
+  //     },
+  //     body: jsonEncode(<String, String>{
+  //       "email": email,
+  //       "alt_mobile_no": mobile_no,
+  //       "password": password,
+  //       "code": "",
+  //       "code_name": ""
+  //     }),
+  //   );
+  //
+  //   if (response.statusCode == 200) {
+  //     _createToast("User registered successfully, Please login");
+  //     Navigator.of(context).pop();
+  //     return RegistrationModel.fromJson(json.decode(response.body));
+  //   } else {
+  //     _createToast("Failed to register User");
+  //     throw Exception('Failed to load album');
+  //   }
+  // }
+
+  void _createToast(String message) {
+    Fluttertoast.showToast(
+        msg: message,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1);
   }
 }
